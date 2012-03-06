04141cf58a99b2148fa9d90b9a905e0f0ade26e0
Pär Gregersson <par@binarypeak.se>
2012-03-06 21:57:08 +0100

    Added the best config evaaah!

---
+++ init.el
@@ -0,0 +1,10 @@
+
+(defun dragons()
+  (interactive)
+  (insert "DRAGONS!!!!!!!!")
+  )
+
+(global-set-key [up] 'dragons)
+(global-set-key [down] 'dragons)
+(global-set-key [left] 'dragons)
+(global-set-key [right] 'dragons)
