e3668faabb1f643b7ab5ba7c8f7efb74fe89b05b
Daniel Appelgren <daniel.appelgren@gmail.com>
2012-03-22 19:18:31 +0100

    Forsta commit av Daniel

--- init.el
+++ init.el
@@ -1,3 +1,5 @@
+(add-to-list 'load-path "~/github/config/lib/")
+(require 'fill-column-indicator)
 
 (defun dragons()
   (interactive)
@@ -8,3 +10,18 @@
 (global-set-key [down] 'dragons)
 (global-set-key [left] 'dragons)
 (global-set-key [right] 'dragons)
+
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(setq indent-line-function 'insert-tab)
+
+;; Show line-number in the mode line
+(line-number-mode 1)
+
+;; Show column-number in the mode line
+(column-number-mode 1)
+
+(setq fci-rule-width 1)
+(setq fci-rule-color "darkblue")
+(setq fci-rule-column 80)
+(add-hook 'javascript-mode-hook 'fci-mode)
