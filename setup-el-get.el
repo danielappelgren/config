e9ff692a36a29fe4e2fd4bfa34c8484085af7c72
Pär Gregersson <par@binarypeak.se>
2012-04-12 09:52:06 +0200

    Removed yasnippet and emacs-goodies.

--- setup-el-get.el
+++ setup-el-get.el
@@ -111,14 +111,14 @@
 ;;
 ;; Note: el-get-install requires git, so we know we have at least that.
 ;;
-(when (el-get-executable-find "cvs")
-  (add-to-list 'my:el-get-packages 'emacs-goodies-el)) ; the debian addons for emacs
+;; (when (el-get-executable-find "cvs")
+;;   (add-to-list 'my:el-get-packages 'emacs-goodies-el)) ; the debian addons for emacs
 
-(when (el-get-executable-find "svn")
-  (loop for p in '(psvn    		; M-x svn-status
-		   yasnippet)		; powerful snippet mode
+;; (when (el-get-executable-find "svn")
+;;   (loop for p in '(psvn    		; M-x svn-status
+;; 		   yasnippet)		; powerful snippet mode
 	 
-	do (add-to-list 'my:el-get-packages p)))
+;; 	do (add-to-list 'my:el-get-packages p)))
 
 
 
