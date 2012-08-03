161af0d0e5f921bcc984f9fc37c237506e93c1e8
Daniel Appelgren <daniel.appelgren@gmail.com>
2012-08-03 13:27:04 +0200

    Clean up el-get setup.

--- init.el
+++ init.el
@@ -55,8 +55,6 @@
 ;; Comment or uncomment region
 (global-set-key (kbd "C-c C-c") 'comment-or-uncomment-region)
 
-(require 'highlight-symbol)
-
 (global-set-key [(control f3)] 'highlight-symbol-at-point)
 (global-set-key [f3] 'highlight-symbol-next)
 (global-set-key [(shift f3)] 'highlight-symbol-prev)
--- setup-el-get.el
+++ setup-el-get.el
@@ -14,57 +14,59 @@
   (url-retrieve
    "https://github.com/dimitri/el-get/raw/master/el-get-install.el"
    (lambda (s)
-     (end-of-buffer)
+     (goto-char (point-max))
      (eval-print-last-sexp))))
 
-;; now either el-get is `require'd already, or have been `load'ed by the
-;; el-get installer.
+(el-get 'sync)
 
 ;; set local recipes
 (setq
  el-get-sources
- '((:name buffer-move			; have to add your own keys
+ '(
+   ;; Easier moving buffers
+   (:name buffer-move ; have to add your own keys
 	  :after (lambda ()
 		   (global-set-key (kbd "<C-S-up>")     'buf-move-up)
 		   (global-set-key (kbd "<C-S-down>")   'buf-move-down)
 		   (global-set-key (kbd "<C-S-left>")   'buf-move-left)
 		   (global-set-key (kbd "<C-S-right>")  'buf-move-right)))
 
-   (:name smex				; a better (ido like) M-x
+   ;; Better M-X
+   (:name smex
    	  :after (lambda ()
    		   (setq smex-save-file "~/.emacs.d/.smex-items")
    		   (global-set-key (kbd "M-x") 'smex)
    		   (global-set-key (kbd "M-X") 'smex-major-mode-commands)))
 
+   ;; Redo
    (:name redo+ :type elpa
           :after (lambda ()
                    ;; Better undo / redo handling
                    (require 'redo+)
                    (global-set-key  [?\M-_] 'redo)))
 
+   ;; Javascript mode
    (:name js2-mode :type git :url "https://github.com/mooz/js2-mode.git" :module "js2-mode")
 
+   ;; Highlight using F3 or C-F3
    (:name highlight-symbol :type http :url "http://nschum.de/src/emacs/highlight-symbol/highlight-symbol.el")
 
    )
  )
 
-
-;; now set our own packages
+;; ;; now set our own packages
 (setq
  my:el-get-packages
  '(el-get				; el-get is self-hosting
    js2-mode
    redo+
    smex
-   json-mode
+   buffer-move
    highlight-symbol
-   ))
+   )
+ )
 
-
-
-
-;; Append the package lists
+;; ;; Append the package lists
 (setq my:el-get-packages
       (append
        my:el-get-packages
