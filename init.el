f527574098427d2d3f52455a66d4b4eb9281d251
Daniel Appelgren <daniel.appelgren@tactel.se>
2014-06-24 11:08:56 +0200

    New config started

--- init.el
+++ init.el
@@ -1,11 +1,5 @@
-(add-to-list 'load-path "~/danielconf/")
-(add-to-list 'load-path "~/danielconf/lib/")
-(add-to-list 'load-path "~/github/config/")
-(add-to-list 'load-path "~/github/config/lib/")
-
-(require 'fill-column-indicator)
-
-(load-library "setup-el-get.el")
+;; (add-to-list 'load-path "~/config/")
+;; (load-library "init")
 
 (setq locale-coding-system 'utf-8)
 (set-terminal-coding-system 'utf-8)
@@ -17,75 +11,100 @@
   (interactive)
   (insert "DRAGONS!!!!!!!!")
   )
-
 (global-set-key [up] 'dragons)
 (global-set-key [down] 'dragons)
 (global-set-key [left] 'dragons)
 (global-set-key [right] 'dragons)
 
-;; Go to next error if available
-(global-set-key (kbd "<f4>") 'next-error)
-;; Go to previous error if available
-(global-set-key (kbd "S-<f4>") 'previous-error)
+;; Overwrite selection when pasting / yanking!
+(delete-selection-mode 1)
 
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 4)
-(setq indent-line-function 'insert-tab)
+;; make searches case insensitive
+(setq case-fold-search t)
 
-;; Show line-number in the mode line
+;; Use y-n instead of yes-no
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(defun comment-or-uncomment-region-or-line ()
+    "Comments or uncomments the region or the current line if there's no active region."
+    (interactive)
+    (let (beg end)
+        (if (region-active-p)
+            (setq beg (region-beginning) end (region-end))
+            (setq beg (line-beginning-position) end (line-end-position)))
+        (comment-or-uncomment-region beg end)))
+(global-set-key (kbd "C-c C-c") 'comment-or-uncomment-region-or-line)
+
+;; Java specefic stuff
+(defun java-shortcuts ()
+  (local-set-key (kbd "C-c C-c") 'comment-or-uncomment-region-or-line))
+(add-hook 'java-mode-hook 'java-shortcuts)
+
+;; HTML specefic stuff
+(defun html-shortcuts ()
+  (local-set-key (kbd "C-c C-c") 'comment-or-uncomment-region-or-line))
+(add-hook 'html-mode-hook 'html-shortcuts)
+
+
+;; Show line and column number in the mode line
 (line-number-mode 1)
-
-;; Show column-number in the mode line
 (column-number-mode 1)
+;; Show line number
+(global-linum-mode t)
 
-;; A library that saves the last position in files
-(require 'saveplace)
-;; Activate it!
-(setq-default save-place t)
+(setq-default tab-width 4)
 
-;; Grep tool goodness
-(require 'grep)
-;; Alias web that is used to search in all web related files
-(add-to-list
- 'grep-files-aliases
- '("web" .  "*.js *.css *.html *.htm *.jsp *.xul")
- )
+;; Remove toolbar icons
+(tool-bar-mode -1)
 
-;; Comment or uncomment region
-(global-set-key (kbd "C-c C-c") 'comment-or-uncomment-region)
+;; Suppress GNU startup message
+(setq inhibit-startup-message t)
 
-(global-set-key [(control f3)] 'highlight-symbol-at-point)
-(global-set-key [f3] 'highlight-symbol-next)
-(global-set-key [(shift f3)] 'highlight-symbol-prev)
-(global-set-key [(meta f3)] 'highlight-symbol-prev)
-
-(defun ask-before-closing ()
-  "Ask whether or not to close, and then close if y was pressed"
-  (interactive)
-  (if (y-or-n-p (format "Are you sure you want to exit Emacs? "))
-      (if (< emacs-major-version 22)
-          (save-buffers-kill-terminal)
-        (save-buffers-kill-emacs))
-    (message "Canceled exit")))
-
-(when window-system
-  (global-set-key (kbd "C-x C-c") 'ask-before-closing))
+;; Show the name of the file being edited in the title bar
+(setq frame-title-format '("%b" (buffer-file-name ": %f")) )
 
 ;; Show parenthesis near cursor
 (show-paren-mode 1)
 
-;; Setup project-root paths and rules
-(load-library "setup-project-root.el")
+;; Resolved by the following steps:
+;; 1. download GnuTLS Windows binaries zip from http://www.gnutls.org/download.html
+;; 2. extract the zip
+;; 3. dump the entire contents of the bin/ folder into C:\Chocolatey\lib\Emacs.24.3\tools\emacs-24.3\bin
 
-;; Show line number
-(global-linum-mode t)
+;; Install and add git to path
 
-;; IDO!
-;; (ido-mode t)
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
 
-;; Set alias to espresso-mode
-(defalias 'js-mode 'espresso-mode)
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+(el-get 'sync)
 
-;; Indentera endast med 4 space för javascript
-;; Fixa linje efter x antal columner
-;; Kan man på något sätt hoppa till en metod i en fil (utan att söka)?
+; extra recipes for packages unknown to el-get (yet)  
+(setq el-get-sources
+      '(
+	(:name highlight-symbol type: git url: "https://github.com/nschum/highlight-symbol.el.git")
+	)
+      )
+
+; list all packages you want installed
+(setq my-el-get-packages
+      (append
+       '(highlight-symbol)
+       (mapcar 'el-get-source-name el-get-sources)
+       )
+      )
+
+(el-get 'sync my-el-get-packages) 
+
+; enable packages
+(require 'highlight-symbol)
+(add-hook 'prog-mode-hook '(lambda () (highlight-symbol-mode t)))
+
+;; solarized theme istalled from package-install
+;; (package-initialize)
+;; (load-theme 'solarized-light t)
