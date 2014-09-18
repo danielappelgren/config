e1967c21ce68e42033e0f763ee19c395cc31ed4c
Daniel Appelgren <daniel.appelgren@gmail.com>
2014-09-18 19:44:30 +0200

    Put el-get setup in a new file.

--- init.el
+++ init.el
@@ -1,3 +1,7 @@
+
+;; First things firts, load el-get packages
+(load-library "setup-el-get.el")
+
 (setq locale-coding-system 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
@@ -63,6 +67,9 @@
 ;; Show parenthesis near cursor
 (show-paren-mode 1)
 
+;; Disable beeping
+(setq visible-bell t)
+
 ;; A library that saves the last position in files
 (require 'saveplace)
 ;; Activate it!
@@ -84,47 +91,3 @@
       auto-save-timeout 20              ; number of seconds idle time before auto-save (default: 30)
       auto-save-interval 200            ; number of keystrokes between auto-saves (default: 300)
       )
-
-;; el-get stuff
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-    (eval-print-last-sexp)))
-
-(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
-(el-get 'sync)
-
-;; extra recipes for packages unknown to el-get (yet)  
-(setq el-get-sources
-      '(
-		(:name highlight-symbol type: git url: "https://github.com/nschum/highlight-symbol.el.git")
-		)
-      )
-
-
-;; list all packages you want installed
-(setq my-el-get-packages
-      (append
-       '(highlight-symbol
-		 multiple-cursors)
-       (mapcar 'el-get-source-name el-get-sources)
-       )
-      )
-
-(el-get 'sync my-el-get-packages) 
-
-;; enable packages
-(require 'highlight-symbol)
-(require 'multiple-cursors)
-(add-hook 'prog-mode-hook '(lambda () (highlight-symbol-mode t)))
-										; Multiple cursors conf
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
-
-;; solarized theme istalled from package-install
-;; (package-initialize)
-;; (load-theme 'solarized-light t)
